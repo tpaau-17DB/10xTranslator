45e36810c3b5b95aab53d03b16f1d5b9fef4ebdd
tpaau-17DB <mikolaj_archusr@tutamail.com>
2024-10-25 22:34:53 +0200

    Cleaned up the code and added a loading bar!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='10x_translator',
+    version='1.0.0',
+    author_email='mikolaj_archusr@tutamail.com',
+    description='The best translator currently aviable.',
+    url='https://github.com/tpaau-17DB/10xTranslator',
+    packages=['10x_translator'],
+    install_requires=['deep_translator']
+)
