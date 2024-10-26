f500e1dc20edb158471e061808928cbbde4f2e17
tpaau-17DB <mikolaj_archusr@tutamail.com>
2024-10-26 20:55:46 +0200

    Added a proper argparsing and made the script actually usable.

---
+++ best_translator/translator.py
@@ -0,0 +1,122 @@
+"""
+The core of the 10xTranslator
+"""
+
+from os import error
+import sys
+import random
+import argparse
+
+import logger as l
+
+if __name__ == "__main__":
+    # If you are using python linters, you'll probably get an error here. Simply ignore it.
+    from utils import translate_once, LANGUAGE_CODES, print_loading_bar, load_file_contents
+else:
+    from .utils import translate_once, LANGUAGE_CODES, print_loading_bar, load_file_contents
+
+
+QUIET_MODE = False
+
+
+def main():
+    """
+    the main funcion
+    gets the program arguments and initiates translation if successful
+    """
+    parser = argparse.ArgumentParser(description='The best translator currently aviable.')
+    parser.add_argument('-q', '--quiet', action='store_true', help='Print only final translation or critical errors.')
+    parser.add_argument('-v', '--verbosity', help='set logger verbosity')
+
+    parser.add_argument('-i', '--iterations', nargs='?', help='Set the "accuracy" of the translation.')
+    parser.add_argument('-e', '--errors_limit', nargs='?', help='Set the maximum number of errors before program gives up. Setting this to -1 removes limit.')
+    parser.add_argument('lang', nargs='?', help='target language')
+    parser.add_argument('file', nargs='?', help='File containing text to translate.')
+
+    args = parser.parse_args()
+
+    file = '/path/to/file.txt'
+    text = 'sample_text'
+    language = 'lang'
+    iterations = 50
+    max_errors = 10
+
+    if args.quiet:
+        l.toggle_logging(False)
+        QUIET_MODE = True
+
+    if args.verbosity:
+        l.log_deb(f"Logger verbosity set to {args.verbosity}")
+        l.set_verbosity(int(args.verbosity))
+
+    if args.iterations:
+        l.log_deb(f"Iteration count set to {args.iterations}.")
+        iterations = args.iterations
+
+    if args.errors_limit:
+        l.log_deb(f"Error limit set to {args.errors_limit}.")
+        max_errors = args.errors_limit
+
+    if args.lang:
+        l.log_deb(f"Target language set to '{args.lang}'.")
+        language = args.lang
+    else:
+        l.log_err("No language specified. Aborting.", override_prior=True)
+        sys.exit(1)
+
+    if args.file:
+        l.log_deb(f"File path set to '{args.file}'.")
+        file = args.file
+    else:
+        l.log_err("No file specified. Aborting.", override_prior=True)
+        sys.exit(1)
+
+    text = load_file_contents(file)
+
+    if not text:
+        l.log_err("Failed to load file.", override_prior=True)
+        sys.exit(1)
+
+    translate(text, language, iterations=iterations, errors_limit=max_errors)
+
+def translate(text, destination, iterations = 50, errors_limit=10):
+    """
+    * The method for text translating
+    * text is the string to be translated
+    * destination is the desired output language
+    * iterations will basically increase the 'accuracy' of the translation,
+    but will also make wait time longer. Values of 50-200 are in reasonable range,
+    depending on your goal
+    """
+    l.log_deb("Starting translation...")
+    dst = ""
+    i = int(iterations)
+    errors = int(errors_limit)
+    while i > 0:
+        if i == 1:
+            dst = destination
+        else:
+            dst = random.choice(LANGUAGE_CODES)
+
+        if not QUIET_MODE:
+            print_loading_bar(i, int(iterations))
+
+        try:
+            text = translate_once(text, dst)
+        except Exception as e:
+            if errors_limit != -1:
+                errors += 1
+            l.log_err("\nFailed to translate due to an error: {e}")
+
+            if errors < errors_limit:
+                continue
+            l.log_err("Too many errors occured, aborting.")
+            return text
+
+        i -= 1
+
+    print(text)
+    return text
+
+if __name__ == '__main__':
+    main()
--- best_translator/utils.py
+++ best_translator/utils.py
@@ -3,8 +3,10 @@ utils for 10xTranslator
 """
 
 import sys
+import os
 
 from deep_translator import GoogleTranslator
+import logger as l
 
 LANGUAGE_CODES = ['af', 'sq', 'am', 'ar', 'hy', 'as', 'ay', 'az', 'bm', 'eu', 'be', 'bn', 'bho', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'dv', 'doi', 'nl', 'en', 'eo', 'et', 'ee', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'ilo', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'rw', 'gom', 'ko', 'kri', 'ku', 'ckb', 'ky', 'lo', 'la', 'lv', 'ln', 'lt', 'lg', 'lb', 'mk', 'mai', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mni-Mtei', 'lus', 'mn', 'my', 'ne', 'no', 'or', 'om', 'ps', 'fa', 'pl', 'pt', 'pa', 'qu', 'ro', 'ru', 'sm', 'sa', 'gd', 'nso', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'ts', 'tr', 'tk', 'ak', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
 
@@ -29,3 +31,19 @@ def print_loading_bar(iteration, iterations_total, length = 30, fill = '#'):
 
     if iteration == 1:
         print()
+
+def load_file_contents(file_path):
+    """
+    loads file based on specified path and returns its contents
+    """
+    expanded_path = os.path.expanduser(file_path)
+
+    try:
+        with open(expanded_path, 'r', encoding='utf-8') as file:
+            contents = file.read()
+        return contents
+    except FileNotFoundError:
+        l.log_err(f"The file at '{expanded_path}' does not exist.", override_prior=True)
+    except IOError as e:
+        l.log_err(f"An error occurred while reading the file: {e}", override_prior=True)
+    return None
